cf93fa0cf516398342a3571ab46d1e24ef1774a8
meghaohm <42211311+meghaohm@users.noreply.github.com>
2022-01-05 14:02:05 -0500

    update

---
+++ Project/legacy/readBiovid.m
@@ -0,0 +1,5 @@
+function file = readBiovid(file_path)
+%readBiovid Summary of this function goes here
+%  Reads biovid data
+file = readtable(file_path);
+end
--- Project/readBiovid.m
+++
@@ -1,5 +0,0 @@
-function file = readBiovid(file_path)
-%readBiovid Summary of this function goes here
-%  Reads biovid data
-file = readtable(file_path);
-end
